11ffd86a51d00136b80fe8bb6aa55e48e1621d85
john.zhen <john.zhen@rpdata.com>
2011-08-24 17:21:46 +1000

    version 1.3

--- hello-world-johnzhen/src/App.java
+++ hello-world-johnzhen/src/App.java
@@ -2,7 +2,7 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-//version 1.2 -- johnzhen
+//version 1.3 -- johnzhen
 public class App {
 
 	/**
