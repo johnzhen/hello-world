f8c53cf74440d24f18c1937e5c90664bfd464cab
johnzhen <john.zhen@rpdata.com>
2011-08-24 20:59:18 +1000

    version 1.8 johnzhen

--- hello-world-johnzhen/src/App.java
+++ hello-world-johnzhen/src/App.java
@@ -2,7 +2,7 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-//version 1.7-- zhiyizhen
+//version 1.8-- johnzhen
 public class App {
 
 	/**
