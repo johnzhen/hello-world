7dcaa95af97c57dfb4997b39ff49656e1a1b5dba
john.zhen <john.zhen@rpdata.com>
2011-08-24 13:12:37 +1000

    development

--- hello-world-johnzhen/src/App.java
+++ hello-world-johnzhen/src/App.java
@@ -6,7 +6,7 @@ public class App {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("abc");
+		System.out.println("abc-development");
 
 	}
 
