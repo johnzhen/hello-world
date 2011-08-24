3528b542871eccd35c78ff0e236afe6f74d5fd20
johnzhen <john.zhen@rpdata.com>
2011-08-25 06:42:16 +1000

    v 1.9

--- hello-world-johnzhen/src/App.java
+++ hello-world-johnzhen/src/App.java
@@ -2,7 +2,7 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-//version 1.8-- johnzhen
+//version 1.9-- johnzhen
 public class App {
 
 	/**
@@ -10,7 +10,7 @@ public class App {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("abc-development");
+		System.out.println("abc-development 1.9");
 		DateFormat formatter;
 		Date saleDateFrom = new Date();
 		Date saleDateTo = new Date();
